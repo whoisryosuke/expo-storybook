5e4894b9d4d66c3a0b41cffd31fd300c8a910318
Ryosuke <ryosuke.san.hana@gmail.com>
2020-09-08 15:10:39 -0700

    Styled components example

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react-native';
-import { Button } from 'react-native';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react-native";
+import { Button } from "react-native";
+import styled from "styled-components/native";
+
+const StyledButton = styled(Button)`
+  background-color: red;
+  color: red;
+  background: red;
+`;
 
 export default {
-  title: 'Button',
+  title: "Button",
 };
 
 export const text = () => (
-  <Button title="Hello Button" onPress={action('clicked')} />
+  <Button title="Hello Button" onPress={action("clicked")} />
+);
+export const styledComponent = () => (
+  <StyledButton title="Hello Button" onPress={action("clicked")} />
 );
 
 // On-Device Register
-storiesOf('Button', module).add('Text', text);
+storiesOf("Button", module).add("Text", text);
